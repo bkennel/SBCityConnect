14432680c748f8bf6556b380e08dc92d4a1414a3
erinflynn <eflynn3@nd.edu>
2017-02-28 23:52:44 -0500

    Correct Photos for First Time User Updated Payment Pages

--- app/src/main/java/mobilend/sbcityconnect/PayItForward1Activity.java
+++ app/src/main/java/mobilend/sbcityconnect/PayItForward1Activity.java
@@ -3,13 +3,89 @@ package mobilend.sbcityconnect;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class PayItForward1Activity extends AppCompatActivity {
+public class PayItForward1Activity extends AppCompatActivity implements android.widget.PopupMenu.OnMenuItemClickListener  {
+    ImageButton menuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payitforward1);
+        //Intent intent = getIntent();
+
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
+        menuButton=(ImageButton) findViewById(R.id.menuButton);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                android.widget.PopupMenu popup = new android.widget.PopupMenu(PayItForward1Activity.this, menuButton);
+                popup.setOnMenuItemClickListener(PayItForward1Activity.this);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+                popup.show();
+            }
+        });
+
     }
-    Intent intent = getIntent();
+
+/*    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }*/
+
+    public boolean onMenuItemClick(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.life:
+                //TODO - implement submenu
+                Toast.makeText(this,"Life",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.calendar:
+                startActivity(new Intent(this, CalendarActivity.class));
+                return true;
+            case R.id.payments:
+                //go to payments
+                startActivity(new Intent(this, EPaymentActivity.class));
+                return true;
+            case R.id.work:
+                Toast.makeText(this,"Work",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.business:
+                Toast.makeText(this,"Business",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.government:
+                Toast.makeText(this,"Government",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.settings:
+                Toast.makeText(this,"Settings",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.logout:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.menuIcon:
+                android.widget.PopupMenu popup = new android.widget.PopupMenu(PayItForward1Activity.this, menuButton);
+                popup.setOnMenuItemClickListener(PayItForward1Activity.this);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+                popup.show();
+                return true;
+        }
+        return false;
+    }
 }
