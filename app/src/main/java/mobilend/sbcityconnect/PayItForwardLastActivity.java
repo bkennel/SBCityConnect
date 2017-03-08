be114ac08cca246f82461a350f023147af087689
erinflynn <eflynn3@nd.edu>
2017-03-08 11:21:22 -0500

    Pay it forward UI

--- app/src/main/java/mobilend/sbcityconnect/PayItForwardLastActivity.java
+++ app/src/main/java/mobilend/sbcityconnect/PayItForwardLastActivity.java
@@ -1,31 +1,22 @@
 package mobilend.sbcityconnect;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Spinner;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class PayItForward1Activity extends AppCompatActivity implements android.widget.PopupMenu.OnMenuItemClickListener  {
+public class PayItForwardLastActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
     ImageButton menuButton;
-    private Spinner addressSpinner, accountSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payitforward1);
-        //Intent intent = getIntent();
+        setContentView(R.layout.activity_pay_it_forward_last);
 
         Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -35,43 +26,13 @@ public class PayItForward1Activity extends AppCompatActivity implements android.
         menuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                android.widget.PopupMenu popup = new android.widget.PopupMenu(PayItForward1Activity.this, menuButton);
-                popup.setOnMenuItemClickListener(PayItForward1Activity.this);
+                PopupMenu popup = new PopupMenu(PayItForwardLastActivity.this, menuButton);
+                popup.setOnMenuItemClickListener(PayItForwardLastActivity.this);
                 popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
                 popup.show();
             }
         });
-        addItemsAddressSpinner();
-        addItemsAccountSpinner();
     }
-    public void addItemsAddressSpinner(){
-        addressSpinner = (Spinner) findViewById(R.id.BillingAddressSpinner);
-        List<String> list = new ArrayList<String>();
-        list.add("Billing Address");
-        list.add("Address 1");
-        list.add("Address 2");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        addressSpinner.setAdapter(dataAdapter);
-    }
-    public void addItemsAccountSpinner(){
-        accountSpinner = (Spinner) findViewById(R.id.PaymentAccountSpinner);
-        List<String> list = new ArrayList<String>();
-        list.add("Payment Account");
-        list.add("Account 1");
-        list.add("Account 2");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        accountSpinner.setAdapter(dataAdapter);
-    }
-/*    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.toolbar_menu, menu);
-        return true;
-    }*/
 
     public boolean onMenuItemClick(MenuItem item){
         switch(item.getItemId()){
@@ -110,8 +71,8 @@ public class PayItForward1Activity extends AppCompatActivity implements android.
     public boolean onOptionsItemSelected(MenuItem item){
         switch(item.getItemId()){
             case R.id.menuIcon:
-                android.widget.PopupMenu popup = new android.widget.PopupMenu(PayItForward1Activity.this, menuButton);
-                popup.setOnMenuItemClickListener(PayItForward1Activity.this);
+                PopupMenu popup = new PopupMenu(PayItForwardLastActivity.this, menuButton);
+                popup.setOnMenuItemClickListener(PayItForwardLastActivity.this);
                 popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
                 popup.show();
                 return true;
@@ -119,8 +80,8 @@ public class PayItForward1Activity extends AppCompatActivity implements android.
         return false;
     }
 
-    public void goToPaymentSummary(View view){
-        Intent intent = new Intent(this, PaymentSummaryActivity.class);
+    public void goBack(View view){
+        Intent intent = new Intent(this, PayItForward1Activity.class);
         startActivity(intent);
     }
 }
